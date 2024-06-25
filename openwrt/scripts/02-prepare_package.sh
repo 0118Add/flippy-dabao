e0862febb2c2c9a69a8222007f57a893270b6ca7
0118Add <60157610+0118Add@users.noreply.github.com>
2024-06-25 16:30:59 +0800

    Add files via upload

---
+++ openwrt/scripts/02-prepare_package.sh
@@ -0,0 +1,140 @@
+#!/bin/bash -e
+
+# golang 1.22
+rm -rf feeds/packages/lang/golang
+git clone https://$github/sbwml/packages_lang_golang -b 22.x feeds/packages/lang/golang
+
+# node - prebuilt
+rm -rf feeds/packages/lang/node
+git clone https://$github/sbwml/feeds_packages_lang_node-prebuilt feeds/packages/lang/node
+
+# Default settings
+git clone https://$github/sbwml/default-settings package/new/default-settings
+
+# boost - 1.84.0
+rm -rf feeds/packages/libs/boost
+cp -a ../master/packages/libs/boost feeds/packages/libs/boost
+
+# lrzsz - 0.12.20
+rm -rf feeds/packages/utils/lrzsz
+git clone https://$github/sbwml/packages_utils_lrzsz package/new/lrzsz
+
+# irqbalance - openwrt master
+rm -rf feeds/packages/utils/irqbalance
+cp -a ../master/packages/utils/irqbalance feeds/packages/utils/irqbalance
+
+# OpenClash
+git clone --depth=1 -b dev https://github.com/vernesong/OpenClash package/new/OpenClash
+
+# autoCore
+git clone https://$github/sbwml/autocore-arm -b openwrt-23.05 package/new/autocore
+
+# ddns-go
+git clone https://github.com/sirpdboy/luci-app-ddns-go package/luci-app-ddns-go
+
+# netkit-ftp
+git clone https://$github/sbwml/package_new_ftp package/new/ftp
+
+# nethogs
+git clone https://github.com/sbwml/package_new_nethogs package/new/nethogs
+
+# SSRP & Passwall
+rm -rf feeds/packages/net/{xray-core,v2ray-core,v2ray-geodata,sing-box}
+git clone https://$github/sbwml/openwrt_helloworld package/new/helloworld -b v5
+
+# DAED
+#git clone https://$github/sbwml/luci-app-daed package/new/daed
+git clone https://github.com/QiuSimons/luci-app-daed-next package/new/daed-next
+
+# immortalwrt homeproxy
+git clone https://$github/immortalwrt/homeproxy package/new/homeproxy
+sed -i "s/ImmortalWrt/OpenWrt/g" package/new/homeproxy/po/zh_Hans/homeproxy.po
+sed -i "s/ImmortalWrt proxy/OpenWrt proxy/g" package/new/homeproxy/htdocs/luci-static/resources/view/homeproxy/{client.js,server.js}
+
+# mihomo
+#git clone https://$github/pmkol/openwrt-mihomo package/new/openwrt-mihomo
+#mkdir -p package/new/openwrt-mihomo/luci-app-mihomo/root/etc/mihomo/run/ui
+#curl -Lso package/new/openwrt-mihomo/luci-app-mihomo/root/etc/mihomo/run/Country.mmdb https://$github/MetaCubeX/meta-rules-dat/releases/download/latest/country-lite.mmdb
+#curl -Lso metacubexd-gh-pages.tar.gz https://$github/MetaCubeX/metacubexd/archive/refs/heads/gh-pages.tar.gz
+#tar zxf metacubexd-gh-pages.tar.gz
+#mv metacubexd-gh-pages package/new/openwrt-mihomo/luci-app-mihomo/root/etc/mihomo/run/ui/metacubexd
+
+# bump geodata version
+#rm -rf package/new/helloworld/v2ray-geodata
+#git clone https://$github/sbwml/v2ray-geodata package/new/helloworld/v2ray-geodata
+#sed -i 's#Loyalsoldier/geoip/releases/latest/download/geoip-only-cn-private.dat#MetaCubeX/meta-rules-dat/releases/download/latest/geoip-lite.dat#g; s#Loyalsoldier/v2ray-rules-dat/releases/latest/download/geosite.dat#MetaCubeX/meta-rules-dat/releases/download/latest/geosite.dat#g' package/new/helloworld/v2ray-geodata/Makefile
+
+# alist
+#git clone https://$github/sbwml/openwrt-alist package/new/alist
+
+# Netdata
+rm -rf feeds/packages/admin/netdata
+cp -a ../master/packages/admin/netdata feeds/packages/admin/netdata
+sed -i 's/syslog/none/g' feeds/packages/admin/netdata/files/netdata.conf
+
+# unblockneteasemusic
+git clone https://$github/UnblockNeteaseMusic/luci-app-unblockneteasemusic package/new/luci-app-unblockneteasemusic
+sed -i 's/解除网易云音乐播放限制/音乐解锁/g' package/new/luci-app-unblockneteasemusic/root/usr/share/luci/menu.d/luci-app-unblockneteasemusic.json
+
+# Theme
+git clone --depth 1 https://$github/sbwml/luci-theme-argon.git package/new/luci-theme-argon
+sed -i 's/Argon 主题设置/主题设置/g' package/new/luci-theme-argon/luci-app-argon-config/po/zh_Hans/argon-config.po
+
+# iperf3
+rm -rf feeds/packages/net/iperf3
+cp -a ../master/packages/net/iperf3 feeds/packages/net/iperf3
+sed -i "s/D_GNU_SOURCE/D_GNU_SOURCE -funroll-loops/g" feeds/packages/net/iperf3/Makefile
+
+# custom packages
+rm -rf feeds/packages/utils/coremark
+git clone https://github.com/8688Add/openwrt_pkgs package/new/custom --depth=1
+# coremark - prebuilt with gcc15
+if [ "$platform" = "rk3568" ]; then
+    curl -s https://$mirror/openwrt/patch/coremark/coremark.aarch64-4-threads > package/new/custom/coremark/src/musl/coremark.aarch64
+elif [ "$platform" = "rk3399" ]; then
+    curl -s https://$mirror/openwrt/patch/coremark/coremark.aarch64-6-threads > package/new/custom/coremark/src/musl/coremark.aarch64
+elif [ "$platform" = "armv8" ]; then
+    curl -s https://$mirror/openwrt/patch/coremark/coremark.aarch64-16-threads > package/new/custom/coremark/src/musl/coremark.aarch64
+fi
+
+# luci-compat - fix translation
+sed -i 's/<%:Up%>/<%:Move up%>/g' feeds/luci/modules/luci-compat/luasrc/view/cbi/tblsection.htm
+sed -i 's/<%:Down%>/<%:Move down%>/g' feeds/luci/modules/luci-compat/luasrc/view/cbi/tblsection.htm
+
+# SQM Translation
+mkdir -p feeds/packages/net/sqm-scripts/patches
+curl -s https://$mirror/openwrt/patch/sqm/001-help-translation.patch > feeds/packages/net/sqm-scripts/patches/001-help-translation.patch
+
+# mjpg-streamer init
+sed -i "s,option port '8080',option port '1024',g" feeds/packages/multimedia/mjpg-streamer/files/mjpg-streamer.config
+sed -i "s,option fps '5',option fps '25',g" feeds/packages/multimedia/mjpg-streamer/files/mjpg-streamer.config
+
+# unzip
+rm -rf feeds/packages/utils/unzip
+git clone https://$github/sbwml/feeds_packages_utils_unzip feeds/packages/utils/unzip
+
+# tcp-brutal
+git clone https://$github/sbwml/package_kernel_tcp-brutal package/kernel/tcp-brutal
+
+# 修改系统文件
+sed -i 's/WireGuard/WiGd状态/g' feeds/luci/protocols/luci-proto-wireguard/root/usr/share/luci/menu.d/luci-proto-wireguard.json
+
+# 更改固件版本信息
+sed -i "s|DISTRIB_DESCRIPTION='.*'|DISTRIB_DESCRIPTION='OpenWrt %V'|g" package/base-files/files/etc/openwrt_release
+
+# 克隆immortalwrt-luci仓库
+git clone --depth=1 -b openwrt-23.05 https://github.com/immortalwrt/luci.git immortalwrt-luci
+cp -rf immortalwrt-luci/applications/luci-app-alist feeds/luci/applications/luci-app-alist
+ln -sf ../../../feeds/luci/applications/luci-app-alist ./package/feeds/luci/luci-app-alist
+cp -rf immortalwrt-luci/applications/luci-app-wechatpush feeds/luci/applications/luci-app-wechatpush
+ln -sf ../../../feeds/luci/applications/luci-app-wechatpush ./package/feeds/luci/luci-app-wechatpush
+#cp -rf immortalwrt-luci/applications/luci-app-ddns-go feeds/luci/applications/luci-app-ddns-go
+#ln -sf ../../../feeds/luci/applications/luci-app-ddns-go ./package/feeds/luci/luci-app-ddns-go
+# 克隆immortalwrt-packages仓库
+git clone --depth=1 -b openwrt-23.05 https://github.com/immortalwrt/packages.git immortalwrt-packages
+cp -rf immortalwrt-packages/net/alist feeds/packages/net/alist
+ln -sf ../../../feeds/packages/net/alist ./package/feeds/packages/alist
+#cp -rf immortalwrt-packages/net/ddns-go feeds/packages/net/ddns-go
+#ln -sf ../../../feeds/packages/net/ddns-go ./package/feeds/packages/ddns-go
+cp -rf immortalwrt-packages/net/dae feeds/packages/net/dae
+ln -sf ../../../feeds/packages/net/dae ./package/feeds/packages/dae
